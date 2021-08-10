a5ca3234a1e29a0a3075451521500655ad03d9f2
Евгений Дружняев <druzhniaev@gmail.com>
2021-08-10 16:16:16 +0600

    Add fonts, basic styles

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,5 @@
+import { HtagProps } from "./Htag.props";
+
+ export const Htag = ({tag, children}: HtagProps): JSX.Element => {
+   return (<h1>{children}</h1>);
+ };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+  <>
+  <Head>
+    <title>Mytop</title>
+    <link rel="icon" href="/favicon.ico"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+  </Head>
   return <Component {...pageProps} />;
+
+  </>
+  );
 }
 
 export default MyApp;
